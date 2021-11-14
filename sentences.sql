cb58e1755cb5df577a36a6539148fedbb3f40a18
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-11-14 14:48:07 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -1,13 +1,34 @@
 
+-- SELECT
 
-# Seleccionar con un limite
+--Seleccionar con un limite
 SELECT * FROM <table> LIMIT 5
 
-# Seleccionar un elemento distinto
+--Seleccionar un elemento distinto
 SELECT DISTINCT <column1, column2, ...> FROM <TABLE>
 
-# Contando
+--Contando
 SELECT COUNT(DISTINCT COUNTRY) FROM [Customers]
 
-# Contando con un alias
+--Contando con un alias
 SELECT COUNT(*) AS <alias> FROM (SELECT DISTINCT(<columna>) FROM <table>)
+
+--CREATE
+
+--Crear una tabla
+CREATE TABLE <name_table>
+(
+  <atributo>  <type_data> <PK, Not Null, Null>
+  Id      char(10)      PRIMARY KEY,
+  Brand   char(10)      NOt NULL,
+  Desc    varchar(750)  NULL,
+)
+
+--INSERT INTO
+
+INSERT INTO <table> (<column1, column2, ....>) VALUES (<value1, value2, ....);
+
+
+
+
+
