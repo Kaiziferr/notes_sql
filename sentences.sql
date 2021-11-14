9e7a00450cfe1a977eae51d60af11804bd566a21
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-11-14 14:16:14 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -1,1 +1,13 @@
 
+
+# Seleccionar con un limite
+SELECT * FROM <table> LIMIT 5
+
+# Seleccionar un elemento distinto
+SELECT DISTINCT <column1, column2, ...> FROM <TABLE>
+
+# Contando
+SELECT COUNT(DISTINCT COUNTRY) FROM [Customers]
+
+# Contando con un alias
+SELECT COUNT(*) AS <alias> FROM (SELECT DISTINCT(<columna>) FROM <table>)
