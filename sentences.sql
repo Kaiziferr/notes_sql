8c598001ab13aa4b5830618f831ea5092238f8e5
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-11-18 21:46:47 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -1,3 +1,101 @@
+/*
+1.	Obtener todas las columnas del consumidor
+2.	Cuantos paises distintos hay en la tabla Customer
+6. 	Obtener el nombre y la ciudad de los consumidores
+
+
+
+8.	Cuente cuantos productos tienen un precio igual a 18
+
+10.	Obtener el nombre del consumidor, así como la fecha e identificación del pedido.
+11.	selecciona todos los clientes y todos los pedidos
+12.	Selecciona todos los pedidos con información del cliente y del remitente
+13. 	Seleccionará a todos los clientes y cualquier pedido que puedan tener, ordenar los registros por medio los nombres de los consumidores.
+14.	Clientes que son de la misma ciudad
+15.	Cuantos productos hay con precio mayor a 15 y menor a 30
+
+20.	Clasifique por tipo y sin repetición todos los clientes y proveedores
+
+25.	Recupere la ciudad de la tabla consumidor y proveedor, no importa los duplicados.
+
+30.	Obtenga los registros de la ciudad, el nombre del contacto y la dirección,  del consumidor y del proveedor respectivamente, los cuales deben
+	de estar ordenado por ciudad y sin registros repetidos
+
+
+45.	Recupere la ciudad y el país de los consumidores y proveedores que provengan de Germanía, sin registros duplicados
+
+
+
+*/
+
+
+-- 1
+SELECT * FROM Customers
+
+-- 2
+SELECT COUNT(DISTINCT(Country)) AS 'N_Country' FROM Customers
+SELECT COUNT(*) FROM (SELECT DISTINCT(Country) FROM Customers)
+
+-- 6
+SELECT CustomerName, City FROM Customers
+
+-- 8
+SELECT COUNT(*) AS 'Producto' FROM Products WHERE price = 18
+
+-- 10
+SELECT o.OrderID, c.CustomerName, o.OrderDate  FROM Orders o INNER JOIN Customers c ON c.CustomerID = o.CustomerID
+
+-- 11
+SELECT Customers.CustomerName, Orders.OrderID
+FROM Customers
+FULL OUTER JOIN Orders ON Customers.CustomerID=Orders.CustomerID
+ORDER BY Customers.CustomerName;
+
+-- 12
+SELECT o.OrderID, c.CustomerName, s.ShipperName 
+FROM ((Orders o INNER JOIN Customers c ON o.CustomerID = c.CustomerID) 
+INNER JOIN Shippers s ON o.ShipperID = s.ShipperID)
+
+-- 13
+SELECT c.CustomerName, o.OrderID FROM Customers c LEFT JOIN Orders o ON c.CustomerID = o.CustomerID ORDER BY c.CustomerName
+
+-- 14
+SELECT a.CustomerName AS CustomerName1, b.CustomerName AS CustomerName2, a.City
+FROM Customers a, Customers b
+WHERE a.CustomerID <> b.CustomerID AND a.City = b.City
+ORDER BY a.City
+
+-- 15
+SELECT COUNT(*) AS 'Producto' FROM Products WHERE price > 15 AND price < 30
+
+-- 20
+SELECT 'Customers' AS Type, ContactName, City, Country FROM Customers UNION SELECT 'Supplier', ContactName, City, Country FROM Suppliers;
+
+--25
+SELECT City FROM Customers UNION ALL SELECT City FROM Suppliers ORDER BY City
+
+--30
+SELECT City FROM Customers UNION SELECT City FROM Suppliers ORDER BY City
+
+--45
+SELECT City, Country FROM Customers WHERE Country = 'Germany' UNION SELECT City, Country FROM Suppliers WHERE Country = 'Germany'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 -- SELECT
 SELECT * FROM <table>
