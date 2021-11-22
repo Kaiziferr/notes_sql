485e7a3362f1019ddd6d9d5417325b8320d64777
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-11-21 22:47:56 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -13,12 +13,13 @@
 12.	Insertar UN registro en consumidores con solo el id y nombre del consumidor en la columna CustomerName
 13.	Contar cuantos registros tienen null en la columna ciudad
 14.	Seleccionar los consumidores que tienen una ciudad asignada
-
-
-
-
-
-
+15.     Actualice el registro 93, modificando los valores nulos
+16.	Elimine los registros de los consumidores, cuyo pais es Alemania
+17. 	Seleccione el producto con el precio mínimo de toda la tabla Products
+18.	Seleccione el producto con el precio máximo de toda la tabla Products
+19.	Saque el promedio total y la suma total del precio de los productos
+20.	Seleccione todos los nombre de contactos que inicien con la a
+21.	
 
 
 
@@ -74,7 +75,7 @@ SELECT COUNT(*) FROM Customers WHERE NOT Country = 'Germany'
 SELECT * FROM Customers ORDER BY Country ASC, CustomerName DESC;
 
 --11
-INSERT INTO Customers VALUES ('0', 'Berny', 'Steven Bernal', 'Cra 4C','Colombia', '753148629', 'Colombia')
+INSERT INTO Customers VALUES ('0', 'Berny', 'Steven Bernal', 'Cra 4C','Cali', '753148629', 'Colombia')
 
 --12
 INSERT INTO Customers ('CustomerID', 'CustomerName') VALUES ('93', 'tEMPORAL')
@@ -84,6 +85,42 @@ SELECT COUNT(*) AS 'Country is null' FROM Customers WHERE Country IS NULL
 
 --14
 SELECT * FROM Customers WHERE City NOT NULL
+
+--15
+UPDATE Customers SET ContactName = 'MR-Temporal', Address = 'Cr 85', City = 'Cali', PostalCode = 14785, Country = 'Colombia' WHERE CustomerID = 93
+
+--16
+DELETE FROM Customers WHERE Country = 'Germany'
+
+--17
+SELECT *, MIN(Price) AS 'Precio minimo' FROM Products
+
+--18
+SELECT *, MAX(Price) AS 'Precio máximo ' FROM Products
+
+--19
+SELECT AVG(Price) AS 'AVG Price', SUM(Price) AS 'SUM Price' FROM Products 
+
+/*
+	%	Representa cero, uno o varios caracteres
+	_	Representa un solo caracter
+	
+	Any Examples
+	
+	|'a%'		|Inicia con a									|
+	|'%a'		|Finaliza con a									|
+	|'%of%'		|Encuentra un valor que contenga la sentencia 'of' en alguna parte		|
+	|'_r%'		|Segunda posicion debe ser 'r'							|
+	|'a__%'		|Encuentra valor que inicia con 'a' y que tenga menos de tres caracteres	|
+	|'a%n'		|Inicia con 'a' y termina con 'n'						|
+*/
+
+--20
+SELECT * FROM Customers WHERE ContactName LIKE 'A%'
+
+--21
+
+
 
 
 
@@ -130,6 +167,8 @@ SELECT City FROM Customers UNION SELECT City FROM Suppliers ORDER BY City
 SELECT City, Country FROM Customers WHERE Country = 'Germany' UNION SELECT City, Country FROM Suppliers WHERE Country = 'Germany'
 
 
+--Pendiente
+-- Wildcards
 
 
 
@@ -145,112 +184,6 @@ SELECT City, Country FROM Customers WHERE Country = 'Germany' UNION SELECT City,
 
 
 
-
--- SELECT
-SELECT * FROM <table>
-
--- LIMIT
-SELECT * FROM <table_name> LIMIT 5
-
--- DISTINCT
-SELECT DISTINCT <column1, column2, ...> FROM <table_name>
-
--- AVERAGE
-SELECT AVG(<column>) AS <alias> FROM <table_name>
-
--- COUNT
---Contando
-SELECT COUNT(DISTINCT <column>) FROM <table_name>
---Contando con un alias
-SELECT COUNT(*) AS <alias> FROM (SELECT DISTINCT(<column>) FROM <table_name>)
-
--- MIN
-SELECT MIN(<column>) AS <alias> FROM <table_name>
-
--- MAX
-SELECT MAX(<column>) AS <alias> FROM <table_name>
-
--- SUM
-SELECT SUM(<column>) AS <alias> FROM <table_name>
-
--- SELECT AVG(<column>) AS <alias>, MAX(<column>) AS <alias>, MIN(<column>) AS <alias>, SUM(<column>) AS <alias> FROM <table_name>
-
--- WHERE
-SELECT * FROM <table_name> WHERE <condition>
-
-/*	
-	|OPERATOR	|DESCRIPTION			|
-	|=		|EQUAL				|
-	|<>,!=		|NOT EQUAL			|
-	|>		|GREATER THAN			|
-	|<		|LESS THAN			|
-	|>=		|GREATER THAN OR EQUAL		|
-	|<=		|LESS THAN OR EQUAL		|
-	|BETWEEN	|				|
-	|IS NULL	|				|
-*/
-
-
--- BETWEEN
-SELECT * FROM <table_name> WHERE <column> BETWEEN <value1> AND <value2>
-
--- NULL
-SELECT * FROM <table_name> WHERE <column> IS NULL
-
--- NOT NULL
-SELECT * FROM <table_name> WHERE <column> NOT NULL
-
--- IN
-SELECT * FROM <table_name> WHERE <column> IN (value1, value2, value3, ...)
-SELECT * FROM <table_name> WHERE <column> IN (SELECT <column> FROM <table_name>)
-
--- LIKE
-SELECT * FROM <table_name> WHERE <column> LIKE <pattern>
-/*
-	%	Representa cero, uno o varios caracteres
-	_	Representa un solo caracter
-	
-	Any Examples
-	
-	|'a%'		|Inicia con a									|
-	|'%a'		|Finaliza con a									|
-	|'%of%'		|Encuentra un valor que contenga la sentencia 'of' en alguna parte		|
-	|'_r%'		|Segunda posicion debe ser 'r'							|
-	|'a__%'		|Encuentra valor que inicia con 'a' y que tenga menos de tres caracteres	|
-	|'a%n'		|Inicia con 'a' y termina con 'n'						|
-*/
-
--- ORDER BY
-SELECT <column1, column2, ...> FROM <table_name> WHERE <condition> ORDER BY <column1, column2, ...> ASC
-SELECT <column1, column2, ...> FROM <table_name> WHERE <condition> ORDER BY <column1, column2, ...> DESC
-
--- MATH OPERATIONS
-/*
-ORDER OF OPERATIONS
-	PARENTHESES
-	EXPONENTS
-	MULTIPLICATION
-	DIVISION
-	ADDITION
-	SUBTRACTION
-*/
-SELECT <column1, column2, ...> <math_operations> AS <alias> FROM <table_name>
-
--- GROUP BY 
-SELECT <funcion_agregada>, <column1, column2, ...> FROM <table_name> GROUP BY <column1>
-SELECT COUNT(CustomerID), Country FROM Customers GROUP BY Country ORDER BY COUNT(CustomerID) DESC
-
--- HAVING
--- La clausula HAVING se agregó a SQL porque la WHERE palabra clave no se puede usar con funciones agregadas.
-
-SELECT <funcion_agregada>, <column1, column2, ...> FROM <table_name> GROUP BY <column1> HAVING <funcion_agregada><condition> 
-SELECT <funcion_agregada>, <column1, column2, ...> FROM <table_name> GROUP BY <column1> HAVING COUNT(<column1>)>5
-SELECT <funcion_agregada> AS <alias>, <column1, column2, ...> FROM <table_name> GROUP BY <column1> HAVING <alias><condition>
---busque los álbumes con 12 o más pistas.
-SELECT COUNT(*) FROM (select count(TrackId) from tracks group by AlbumId HAVING Count(TrackId)>11)
---muestra el número de pedidos realizados por cada cliente (pista: se encuentra en la tabla de facturas) y ordena el resultado por el número de pedidos en orden descendente .
-select count(*), * from invoices group by CustomerId order by count(InvoiceId) desc
---https://ucde-rey.s3.amazonaws.com/DSV1015/ChinookDatabaseSchema.png
 
 
 --CREATE
