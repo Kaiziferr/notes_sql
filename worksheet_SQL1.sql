40b347593f0fb0cfeb6c741d2232c28d9b7e0ee4
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2021-12-03 22:34:58 -0500

    Update worksheet_SQL1.sql

--- worksheet_SQL1.sql
+++ worksheet_SQL1.sql
@@ -15,6 +15,58 @@ SELECT * FROM dsv1069.users u INNER JOIN dsv1069.orders o ON u.id = o.user_id
 SELECT COUNT(DISTINCT event_id) FROM dsv1069.events WHERE event_name = 'view_item' 
 
 -- 5. Goal:Compute the number of items in the items table which have been ordered. The query
-below runs, but it isn’t right. Determine what is wrong and correct the error or start from scratch.
 
-SELECT COUNT(DISTINCT orders.item_id) FROM dsv1069.orders
+
+
+-- below runs, but it isn’t right. Determine what is wrong and correct the error or start from scratch.
+
+SELECT 
+  DATE(event_time) AS date,
+  COUNT(*)         AS rows
+FROM 
+  dsv1069.events_201701
+GROUP BY
+  DATE(event_time)
+
+-- Goal: Write a query to format the view_item event into a table with the appropriate columns
+
+SELECT
+  event_id,
+  event_time,
+  user_id
+  platform,
+  (CASE WHEN parameter_name = 'item_id'
+        THEN CAST(parameter_value AS INT)
+        ELSE NULL
+        END) AS item_id
+  FROM
+    dsv1069.events 
+  WHERE
+    event_name = 'view_item'
+  ORDER BY event_id LIMIT 10
+  
+  -- Goal: Use the result from the previous exercise, but make sure
+
+  
+  SELECT 
+  event_id,
+  event_time,
+  user_id,
+  platform,
+  MAX(CASE WHEN parameter_name = 'item_id'
+        THEN CAST(parameter_value AS INT)
+        ELSE NULL 
+        END) AS item_id,
+  MAX(CASE WHEN parameter_name = 'referrer'
+        THEN parameter_value
+        ELSE NULL 
+        END) AS referrer
+  FROM 
+    dsv1069.events 
+  WHERE 
+    event_name = 'view_item'
+  GROUP BY 
+    event_id,
+    event_time,
+    user_id,
+    platform
