1aa86e0218db74621095d98de507319dc1351829
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-01-30 14:49:09 -0500

    Update sentences.sql

--- sentences.sql
+++ sentences.sql
@@ -1,25 +1,25 @@
 /*
-1.	Obtener todas las columnas del consumidor
-2.	Obtener el nombre y la ciudad de los consumidores
-3.	Cuantos países distintos hay en la tabla Customer
-4.	Cuente cuantos productos tienen un precio igual a 18
-5.	Cuantos productos hay con precio mayor a 15 y menor a 30
-6. 	Selecciones todos los registro, cuya identificación sea distinta a módulo 10
-7.	Cuantos proveedores  hay que sean provenientes de Reino unido (UK) y de la ciudad Manchester
-8.	Seleccione los proveedores de Japón o de España
-9. 	Cuantos consumidores no son de Alemania (Germany)
-10.	Ordene los registros de consumidores por pais y nombre, de manera ascendente y descendente respectivamente
-11.	Insertar un registro en los consumidores
-12.	Insertar UN registro en consumidores con solo el id y nombre del consumidor en la columna CustomerName
-13.	Contar cuantos registros tienen null en la columna ciudad
-14.	Seleccionar los consumidores que tienen una ciudad asignada
-15.     Actualice el registro 93, modificando los valores nulos
-16.	Elimine los registros de los consumidores, cuyo pais es Alemania
-17. 	Seleccione el producto con el precio mínimo de toda la tabla Products
-18.	Seleccione el producto con el precio máximo de toda la tabla Products
-19.	Saque el promedio total y la suma total del precio de los productos
-20.	Seleccione todos los nombre de contactos que inicien con la a
-21.	Seleccionar el nombre del consumidor y el nombre del contacto, formatéelos en una sola columna
+1.	Obtener todas las columnas del consumidor.
+2.	Obtener el nombre y la ciudad de los consumidores.
+3.	Cuantos países distintos hay en la tabla consumidor.
+4.	Cuente cuantos productos tienen un precio igual a 18.
+5.	Cuantos productos hay con precio mayor a 15 y menor a 30.
+6. 	Selecciones todos los registro, cuya identificación sea distinta a módulo 10.
+7.	Cuantos proveedores hay que sean provenientes de Reino unido (UK) y de la ciudad Manchester.
+8.	Seleccione los proveedores de Japón o de España.
+9. 	Cuantos consumidores no son de Alemania (Germany).
+10.	Ordene los registros de consumidores por pais y nombre, de manera ascendente y descendente respectivamente.
+11.	Insertar un registro en los consumidores.
+12.	Insertar UN registro en consumidores con solo el id y nombre del consumidor en la columna CustomerName.
+13.	Contar cuantos registros tienen null en la columna ciudad.
+14.	Seleccionar los consumidores que tienen una ciudad asignada.
+15.     Actualice el registro n(Cualquiera), modificando los valores.
+16.	Elimine los registros de los consumidores, cuyo pais es Alemania.
+17. 	Seleccione el producto con el precio mínimo de toda la tabla Products.
+18.	Seleccione el producto con el precio máximo de toda la tabla Products.
+19.	Saque el promedio total y la suma total del precio de los productos.
+20.	Seleccione todos los nombreS de contactos que inicien con la a.
+21.	Seleccionar el nombre del consumidor y el nombre del contacto, formatéelos en una sola columna.
 22.	Seleccione el prefijo de la columna CustomerName de la tabla customer. El prefijo de tener tres caracteres.
 23. 	Seleccione el sufijo de la columna CustomerName de la tabla customer. El sufijo de tener tres caracteres.
 24.	En las órdenes obtener la fecha completa, el año, el mes y el día
@@ -80,10 +80,10 @@ SELECT COUNT(*) AS 'Products' FROM Products WHERE Price = 18
 SELECT COUNT(*) AS 'Products' FROM Products WHERE Price > 15 AND Price < 20
 
 --6
-SELECT * FROM Customers WHERE CustomerID % 2 <> 0
+SELECT * FROM Customers WHERE CustomerID % 10 <> 0
 
 --7
-SELECT COUNT(*) AS 'Country' FROM Suppliers where Country = 'UK' AND City = 'Manchester'
+SELECT COUNT(*) AS 'Number Country' FROM Suppliers where Country = 'UK' AND City = 'Manchester'
 
 --8
 SELECT * FROM Suppliers WHERE Country = 'Japan' or Country = 'Spain'
@@ -95,7 +95,7 @@ SELECT COUNT(*) FROM Customers WHERE NOT Country = 'Germany'
 SELECT * FROM Customers ORDER BY Country ASC, CustomerName DESC;
 
 --11
-INSERT INTO Customers VALUES ('0', 'Berny', 'Steven Bernal', 'Cra 4C','Cali', '753148629', 'Colombia')
+INSERT INTO Customers VALUES ('0', 'Berny', 'Steven', 'Cra 4C','Bogota', '753148629', 'Colombia')
 
 --12
 INSERT INTO Customers ('CustomerID', 'CustomerName') VALUES ('93', 'tEMPORAL')
@@ -107,7 +107,8 @@ SELECT COUNT(*) AS 'Country is null' FROM Customers WHERE Country IS NULL
 SELECT * FROM Customers WHERE City NOT NULL
 
 --15
-UPDATE Customers SET ContactName = 'MR-Temporal', Address = 'Cr 85', City = 'Cali', PostalCode = 14785, Country = 'Colombia' WHERE CustomerID = 93
+UPDATE Customers SET CustomerName = 'new customer name', ContactName = 'new contact name', Address = 'new address', 
+City = 'new city', PostalCode = 'new PostalCode', country = 'new country' WHERE CustomerID = 2
 
 --16
 DELETE FROM Customers WHERE Country = 'Germany'
@@ -139,7 +140,7 @@ SELECT AVG(Price) AS 'AVG Price', SUM(Price) AS 'SUM Price' FROM Products
 SELECT * FROM Customers WHERE ContactName LIKE 'A%'
 
 --21
-SELECT CustomerName || ' (' || ContactName || ')' AS 'Names' FROM Customers
+SELECT '('|| CustomerName ||' '||ContactName || ')' AS 'Names' FROM Customers
 
 --22
 SELECT CustomerName, SUBSTR(CustomerName, 1,3) AS 'PREFIJO' FROM Customers
