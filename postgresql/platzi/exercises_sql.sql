80d633c7d56bb94bbd1ae2751808c612a21b3f12
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-12 22:02:51 -0500

    Update exercises_sql.sql

--- postgresql/platzi/exercises_sql.sql
+++ postgresql/platzi/exercises_sql.sql
@@ -3,5 +3,6 @@
 2. Obtener los cinco primeros registros de la tabla alumno.
 3. Obtener de las colegiaturas unicas, la segunda más costosa
 4. Obtener la segunda mitad de la tabla
+5. Obtener los alumnos con el id 10, 80, 150, 300, 600, 800
 
 */
