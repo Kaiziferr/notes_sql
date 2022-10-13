f6cd82c66ef27f3d27444aa4abdb7eda0ce0bde7
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-12 21:44:31 -0500

    Update answer_sql.sql

--- postgresql/platzi/answer_sql.sql
+++ postgresql/platzi/answer_sql.sql
@@ -6,3 +6,25 @@ SELECT * FROM platzi.alumnos limit 1;
 SELECT * FROM (
 	SELECT ROW_NUMBER() OVER() AS row_id, * FROM platzi.alumnos) 
 AS alumnos_with_arow_nums WHERE row_id = 1;
+
+-- 2
+SELECT * FROM platzi.alumnos FETCH FIRST 5 ROWS ONLY;
+
+SELECT * FROM platzi.alumnos limit 5;
+
+SELECT * FROM (
+	SELECT ROW_NUMBER() OVER() AS row_id, * FROM platzi.alumnos) 
+AS alumnos_with_arow_nums WHERE row_id < 6;
+
+--3
+SELECT DISTINCT colegiatura
+FROM platzi.alumnos AS a1
+WHERE 2 = (
+SELECT COUNT(DISTINCT colegiatura) 
+FROM platzi.alumnos a2
+where a1.colegiatura <= a2.colegiatura
+);
+
+SELECT DISTINCT colegiatura
+FROM platzi.alumnos as a1 order by 
+colegiatura desc OFFSET 1;
