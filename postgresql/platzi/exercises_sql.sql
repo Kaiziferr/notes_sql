cd21086ef614b3e9516146e72d3ad830be38f06f
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-12 21:44:29 -0500

    Update exercises_sql.sql

--- postgresql/platzi/exercises_sql.sql
+++ postgresql/platzi/exercises_sql.sql
@@ -2,5 +2,6 @@
 1. Obtener el primer registro
 2. Obtener los cinco primeros registros de la tabla alumno.
 3. Obtener de las colegiaturas unicas, la segunda más costosa
+4. Obtener la segunda mitad de la tabla
 
 */
