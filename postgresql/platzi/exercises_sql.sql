7b485f175c5465565987cd77e1cfc596488fde2b
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-14 17:10:15 -0500

    Update exercises_sql.sql

--- postgresql/platzi/exercises_sql.sql
+++ postgresql/platzi/exercises_sql.sql
@@ -1,13 +1,14 @@
 /*
 1. Obtener el primer registro
 2. Obtener los cinco primeros registros de la tabla alumno.
-3. Obtener de las colegiaturas unicas, la segunda más costosa
+3. Obtener de las colegiaturas únicas, la segunda más costosa
 4. Obtener la segunda mitad de la tabla
 5. Obtener los alumnos con el id 10, 80, 150, 300, 600, 800
 6. Obtener los alumnos del profesor con id 30
-7. Optener alumnos que no sean del profesor con el id 30
+7. Obtener alumnos que no sean del profesor con el id 30
 8. Extraer las fecha de ingreso del alumno.
-9. Optener el años, mes y día de la incorporación del estudiante.
-10. Optener la hora, minutos y segundo de la fecha de incorporacion.
+9. Obtener el año, mes y día de la incorporación del estudiante.
+10. Obtener la hora, minutos y segundo de la fecha de incorporación.
 11. Seleccionar los alumnos cuyo año de ingreso es el 2019.
+12. Concatenes los valores de cada registro
 */
