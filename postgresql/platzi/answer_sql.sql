ad718934f7ae9d1da46b1f94b672c5ed8f064290
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-14 14:32:32 -0500

    Update answer_sql.sql

--- postgresql/platzi/answer_sql.sql
+++ postgresql/platzi/answer_sql.sql
@@ -54,3 +54,6 @@ SELECT * FROM
 --8
 SELECT EXTRACT (YEAR FROM fecha_incorporacion) AS anio_incorporacion FROM platzi.alumnos;
 SELECT DATE_PART('YEAR', fecha_incorporacion) AS anio_incorporacion FROM platzi.alumnos;
+
+--9
+SELECT DATE_PART('YEAR', fecha_incorporacion) AS anio_incorporacion, DATE_PART('MONTh', fecha_incorporacion) AS mes_incorporacion, DATE_PART('DAY', fecha_incorporacion) AS dia_incorporacion FROM platzi.alumnos;
