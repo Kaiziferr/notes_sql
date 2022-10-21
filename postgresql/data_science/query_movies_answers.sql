acd979284e6401b07d697d8b967363ee806f160a
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-21 11:35:56 -0500

    Update query_movies_answers.sql

--- postgresql/data_science/query_movies_answers.sql
+++ postgresql/data_science/query_movies_answers.sql
@@ -1,1 +1,8 @@
-
+-- 1.
+SELECT MAX(ultima_actualizacion) AS fecha_ultima_actualizacion,
+	clasificacion,
+ 	COUNT(*) AS cantidad_peliculas
+FROM peliculas
+WHERE duracion_renta > 3
+GROUP BY clasificacion, ultima_actualizacion
+ORDER BY fecha_ultima_actualizacion;
