765f43ab0218f9155b6cd48468fbcfa19f566063
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-21 13:45:04 -0500

    Update query_movies_answers.sql

--- postgresql/data_science/query_movies_answers.sql
+++ postgresql/data_science/query_movies_answers.sql
@@ -6,3 +6,13 @@ FROM peliculas
 WHERE duracion_renta > 3
 GROUP BY clasificacion, ultima_actualizacion
 ORDER BY fecha_ultima_actualizacion;
+
+--2
+CREATE OR REPLACE PROCEDURE test_drpcreate_procedure()
+LANGUAGE SQL 
+AS $$
+	DROP TABLE IF EXISTS aaa;
+	CREATE TABLE aaa (bbb char(5) CONSTRAINT firstkey PRIMARY KEY);
+$$; 
+-- Llama a la función en la parte del codigo.
+CALL test_drpcreate_procedure();
