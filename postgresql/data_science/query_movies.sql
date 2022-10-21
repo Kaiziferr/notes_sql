8506f458f211986e54e9f318264d75b3430f1def
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-21 13:43:52 -0500

    Update query_movies.sql

--- postgresql/data_science/query_movies.sql
+++ postgresql/data_science/query_movies.sql
@@ -1,3 +1,4 @@
 /*
 * 1. Seleccionar la fecha de la ultima actualización, clasificación y cantidad de pelicualas. Agrupe por clasificación y la fecha de ultima actualización.
+* 2. Crear un procedimiento que elimine una tabla y luego la cree.
 */
