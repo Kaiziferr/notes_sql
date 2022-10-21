417686a7e1b0deca2f48ffc11c966a861ba35f45
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2022-10-21 13:57:58 -0500

    Update query_movies_answers.sql

--- postgresql/data_science/query_movies_answers.sql
+++ postgresql/data_science/query_movies_answers.sql
@@ -16,3 +16,18 @@ AS $$
 $$; 
 -- Llama a la función en la parte del codigo.
 CALL test_drpcreate_procedure();
+
+--3
+CREATE OR REPLACE FUNCTION test_drpcreate_function() 
+RETURNS VOID
+LANGUAGE plpgsql
+AS $$ 
+BEGIN
+	DROP TABLE IF EXISTS aaa;
+	CREATE TABLE aaa (bbb char(5) CONSTRAINT firstkey PRIMARY KEY, ccc char(5));
+	DROP TABLE IF EXISTS aaab;
+	CREATE TABLE aaaB (bbba char(5) CONSTRAINT secondkey PRIMARY KEY, ccca char(5));
+END
+$$
+
+SELECT test_drpcreate_function();
