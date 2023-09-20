f0a7aef411505dde058f1f31adeb7f4be171d5c0
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-20 09:34:19 -0500

    Update system_procedures.sql

--- microsoft_sql_server/procedures/system_procedures.sql
+++ microsoft_sql_server/procedures/system_procedures.sql
@@ -1,3 +1,6 @@
 -- Permite ver configuración inicial del proceso
 sp_help table;
 sp_help Procedure;
+
+-- Permite ver la codificacion del procedimiento | por el momento solo funciona en procedimientos
+sp_helptext SP_paciente;
