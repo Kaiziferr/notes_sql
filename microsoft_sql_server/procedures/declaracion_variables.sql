ef49230f976bc31f7f3f372bc0556ef89581e207
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-20 10:04:30 -0500

    Create declaracion_variables.sql

---
+++ microsoft_sql_server/procedures/declaracion_variables.sql
@@ -0,0 +1,65 @@
+-- DECLARE VARIABLES
+--______________________________________________________
+
+DECLARE @variableA CHAR(1) = 'a'; -- DECLATE AN VARIABLE
+PRINT @variableA;
+SET @variableA = 'A'; -- UPDATE THE VARIABLE
+PRINT @variableA; -- PRINT to CMD
+
+
+DECLARE @variableB CHAR(1);
+DECLARE @variableC CHAR(2);
+SET @variableB = ISNULL(@variableC, 'N'); -- IF IS NULL THEN SET OTHER VALUE
+
+PRINT @variableB;
+
+-- DECLARE @variableB CHAR(1)
+
+-- Example for declare var
+
+DECLARE @entero INT
+
+SET @entero = NULL
+
+SELECT ISNULL(@entero, 0) AS Valor
+
+PRINT @entero;
+
+
+DECLARE @decimal DECIMAL(10,2)
+
+SET @decimal = NULL
+
+SELECT ISNULL(@decimal, 0.00) AS Valor
+
+
+
+DECLARE @cadena VARCHAR(50)
+
+SET @cadena = NULL
+
+SELECT ISNULL(@cadena, 'Texto predeterminado') AS Valor
+
+
+
+DECLARE @fecha DATETIME
+
+SET @fecha = NULL
+
+SELECT ISNULL(@fecha, GETDATE()) AS Valor
+
+
+
+DECLARE @booleano BIT
+
+SET @booleano = NULL
+
+SELECT ISNULL(@booleano, 0) AS Valor
+
+
+
+DECLARE @moneda MONEY
+
+SET @moneda = NULL
+
+SELECT ISNULL(@moneda, 0.00) AS Valor
