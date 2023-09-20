07f60b301d40f6dfcc8611793b8716e4473fecfb
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-19 22:40:19 -0500

    Update insert.sql

--- microsoft_sql_server/procedures/insert.sql
+++ microsoft_sql_server/procedures/insert.sql
@@ -25,3 +25,48 @@ ELSE
 	BEGIN
 		PRINT 'PACIENTE NO INGRESADO POR QUE YA EXISTE'
 	END
+
+
+
+/*SELECT * FROM CentroMedico.dbo.Turno;
+SELECT * FROM CentroMedico.dbo.TurnoEstado;
+SELECT * FROM CentroMedico.dbo.TurnoPaciente;
+SELECT * FROM CentroMedico.dbo.Medico;
+*/
+
+EXEC IT_TurnoEstado '20200505 13:00',7,2,'Paciente en ayunas'
+
+ALTER PROC IT_TurnoEstado(
+	@fehaTurno CHAR(14), --20230505 12:00
+	@idPaciente  INT,
+	@idmedico INT,
+	@observacion VARCHAR(100) = ''
+)
+
+AS
+
+/*
+ESTADO = 0 (pendiente)
+ESTADO = 1 (realizado)
+ESTADO = 2 (cancelado)
+*/
+
+IF NOT EXISTS(SELECT TOP 1 idTurno FROM CentroMedico.dbo.TURNO 
+	WHERE fechaTurno = @fehaTurno AND estado NOT IN (1, 2))
+	BEGIN
+		INSERT INTO CentroMedico.dbo.TURNO(fechaTurno, estado, observacion) VALUES (
+			@fehaTurno, 0, @observacion
+		)
+		DECLARE @auxIdTurno INT;
+		SET @auxIdTurno = @@IDENTITY
+
+		INSERT INTO CentroMedico.dbo.TurnoPaciente(idTurno, idPaciente, idMedico)
+		VALUES (@auxIdTurno, @idPaciente, @idmedico)
+
+		PRINT 'EL turno se agrego correctamente'
+		RETURN
+	END
+ELSE
+	BEGIN
+		PRINT 'El turno no existe'
+	END
