793882ea0e34a5eec5c832ed273c080f816b459f
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-21 17:37:01 -0500

    Update exercises1.sql

--- microsoft_sql_server/ejercicios/exercises1.sql
+++ microsoft_sql_server/ejercicios/exercises1.sql
@@ -8,5 +8,32 @@ PRINT LEFT(@name, 3)
 PRINT RIGHT(@lastname, 3)
 
 
+-- 2. Declare una variable, y determine la longitud de sus caracteres
+DECLARE @name VARCHAR(10);
+SET @name = 'steven'
+PRINT CONCAT(@name,', ',LEN(@name))
 
+-- 3. Declare dos variables (nombre, apellido), al nombre ponerlo en minúscula y al apellido en mayúscula.
+DECLARE @name VARCHAR(10);
+DECLARE @lastname VARCHAR(10);
 
+SET @name = 'TOM';
+SET @lastname = 'jerry';
+
+PRINT UPPER(@lastname) + ' ' + LOWER(@name)
+
+-- 4. Declare una variable donde hallan tres variables separadas por espacio, remplace el espacio por guion bajo.
+DECLARE @texto VARCHAR(15); 
+SET @texto = 'Leche Pan Carne';
+SELECT REPLACE(@texto, ' ', '_')
+
+--5. Defina una variable con un valor, y agréguele cinco ceros por delante
+
+DECLARE @texto VARCHAR(5);
+SET @texto = 'AK865';
+PRINT CONCAT(REPLICATE('0', 5), @texto ) 
+
+  
+DECLARE @texto VARCHAR(5);
+SET @texto = 'ALTA';
+PRINT REPLICATE(@texto, 5)
