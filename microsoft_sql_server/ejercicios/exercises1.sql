5c43b8a5ecbfb7dd2770e0ae04115570302b94fd
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-24 18:40:57 -0500

    Update exercises1.sql

--- microsoft_sql_server/ejercicios/exercises1.sql
+++ microsoft_sql_server/ejercicios/exercises1.sql
@@ -37,3 +37,40 @@ PRINT CONCAT(REPLICATE('0', 5), @texto )
 DECLARE @texto VARCHAR(5);
 SET @texto = 'ALTA';
 PRINT REPLICATE(@texto, 5)
+
+--6. Obtener la fecha del servidor y la del meridiano.
+SELECT GETDATE() -- HORA DEL SISTEMA DONDE ESTA INSTALADO EL MOTOR
+SELECT GETUTCDATE() -- HORA DEL MERIDIANO
+
+--7. Sumar dos años a la fecha actual, restar 3 meses y restar 30 minutos
+SELECT DATEADD(year, 2, GETDATE())
+SELECT DATEADD(mm, -3, GETDATE())
+SELECT DATEADD(mi, -30, GETDATE())
+
+--8. Sacar la diferencia entre la fecha actual y una del futuro. Obtener la diferencia en minutos y días de la fecha del ordenador y la del meridiano
+SELECT DATEDIFF(year, GETDATE(), '20291103')
+SELECT DATEDIFF(mi, GETUTCDATE(), GETDATE())
+SELECT DATEDIFF(dd, GETUTCDATE(), GETDATE())
+
+--9 Obtener partes del día
+
+SELECT DATEPART(day, GETDATE())
+SELECT DATEPART(dw, GETDATE())
+SELECT DATEPART(DAYOFYEAR, GETDATE())
+SELECT DATEPART(year, GETDATE())
+SELECT DATEPART(month, GETDATE())
+
+-- 10 Imprimir un mensaje si es una fecha
+IF ISDATE('20290132') = 1
+	PRINT 'IS DATE'
+
+ELSE 
+	PRINT 'NOT IS DATE'
+
+--11 Castee en tiempo de ejecución el valor de una variable y el valor de una tabla
+DECLARE @moneyy FLOAT;
+SET @moneyy = 25.8;
+SELECT CAST(@moneyy AS INT) AS MONEDA;
+
+
+SELECT CAST(idPaciente as money) AS FLOAT FROM CentroMedico.dbo.Paciente;
