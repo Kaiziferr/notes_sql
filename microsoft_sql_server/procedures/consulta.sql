cdb502ef5ee8afc8492b2f30a68ff8b6ae944385
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-09-29 21:35:36 -0500

    Update consulta.sql

--- microsoft_sql_server/procedures/consulta.sql
+++ microsoft_sql_server/procedures/consulta.sql
@@ -10,3 +10,32 @@ CREATE PROC SP_paciente ( --S: select P: procedure
 AS 
 
 SELECT * FROM Paciente WHERE idPaciente = @idPaciente;
+
+---------------------------------------------------------------------------------------
+ALTER PROC SELECT_TurnoPaciente(
+	@idpaciente INT
+)
+
+AS
+
+SET NOCOUNT ON
+
+/*
+SELECT * FROM CentroMedico.dbo.Especialidad;
+
+SELECT * FROM CentroMedico.dbo.MedicoEspecialidad;
+INSERT INTO CentroMedico.dbo.MedicoEspecialidad VALUES (2, 2, '')
+*/
+
+SELECT * FROM CentroMedico.dbo.Paciente P
+INNER JOIN CentroMedico.dbo.TurnoPaciente TP
+ON P.idPaciente = TP.idPaciente 
+INNER JOIN CentroMedico.dbo.Turno T
+ON T.idTurno = TP.idTurno
+INNER JOIN CentroMedico.dbo.MedicoEspecialidad M
+ON M.idMedico = TP.idMedico
+WHERE P.idPaciente = @idpaciente;
+
+
+
+EXEC SELECT_TurnoPaciente 7;
