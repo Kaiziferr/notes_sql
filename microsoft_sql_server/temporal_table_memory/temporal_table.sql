a6b052f652f7edb17d272a059ffcf5265c0eeae2
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-10-02 21:48:07 -0500

    Update temporal_table.sql

--- microsoft_sql_server/temporal_table_memory/temporal_table.sql
+++ microsoft_sql_server/temporal_table_memory/temporal_table.sql
@@ -9,3 +9,20 @@ INSERT INTO @mitabla VALUES('COLOMBIA')
 INSERT INTO @mitabla VALUES('ECUADOR')
 
 SELECT * FROM @mitabla;
+
+
+-- CREATE TABLE
+CREATE TABLE #mitabla(
+	id int IDENTITY(1,1),
+	name VARCHAR(50), 
+	lastname VARCHAR(50))
+
+INSERT INTO #mitabla VALUES('KIRBY', 'STAR')
+INSERT INTO #mitabla VALUES('LUIGI', 'BROS')
+INSERT INTO #mitabla VALUES('SAMUS', 'ARAN')
+
+SELECT * FROM #mitabla;
+
+-- DELATE TABLE
+DROP TABLE #mitabla
+
