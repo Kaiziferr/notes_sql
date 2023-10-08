abadd6f18f483040fe4ed04e822aa98be698263a
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-10-08 16:13:27 -0500

    Update TRIGGER.sql

--- microsoft_sql_server/TRIGGER/TRIGGER.sql
+++ microsoft_sql_server/TRIGGER/TRIGGER.sql
@@ -13,3 +13,28 @@ SELECT * FROM Paciente;
 INSERT INTO Paciente VALUES(123456789, 'Monfy', 'Bernal', '20150505', 'cra','COL','' ,'', 'neL')
 
 SELECT * FROM PacienteLog;
+
+
+
+
+CREATE TRIGGER PacientesModificados ON Paciente
+AFTER UPDATE
+AS
+
+IF EXISTS(SELECT idPaciente FROM PacienteLog
+			WHERE idPaciente = (SELECT idPaciente FROM INSERTED))
+
+
+	UPDATE PacienteLog SET fechaModificacion = GETDATE()
+	WHERE idPaciente = (SELECT idPaciente FROM INSERTED)
+
+ELSE 
+	INSERT INTO PacienteLog (idPaciente, idPais, fechaModificacion)
+	SELECT idPaciente, idPais, GETDATE() FROM INSERTED
+
+
+select * from Paciente
+
+select * from PacienteLog
+
+UPDATE Paciente SET dni = 147852369 where idPaciente = 7; 
