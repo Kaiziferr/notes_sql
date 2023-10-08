98f1af0d4a8725d379656ac188bf4f7b060b9cac
Steven Bernal Tovar <36647154+Kaiziferr@users.noreply.github.com>
2023-10-08 16:22:52 -0500

    Update TRIGGER.sql

--- microsoft_sql_server/TRIGGER/TRIGGER.sql
+++ microsoft_sql_server/TRIGGER/TRIGGER.sql
@@ -38,3 +38,22 @@ select * from Paciente
 select * from PacienteLog
 
 UPDATE Paciente SET dni = 147852369 where idPaciente = 7; 
+
+
+
+CREATE TRIGGER PacienteBaja ON Paciente
+FOR DELETE
+AS
+-- AFTER DELETE -- ELIMINA EN LA TABLA QUE ALMACENA O HACE CAMBIOS
+
+IF EXISTS(SELECT idPaciente FROM PacienteLog
+			WHERE idPaciente = (SELECT idPaciente FROM deleted))
+
+	UPDATE PacienteLog SET fechaBaja = GETDATE()
+	WHERE idPaciente = (SELECT idPaciente FROM deleted)
+
+ELSE 
+	INSERT INTO PacienteLog (idPaciente, idPais, fechaBaja)
+	SELECT idPaciente, idPais, GETDATE() FROM deleted
+
+DELETE FROM Paciente where idPaciente = 22; 
